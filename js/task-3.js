0c7059e6d89c5ed7a6bd1f12fe8ba33c83a5eab1
Игорь Наумов <igornaumovo@gmail.com>
2024-12-03 14:39:55 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 "use strict";
 const sortByDescendingFriendCount = users => 
-  [...users].sort((a, b) => b.friends.length - a.friends.length);
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
@@ -78,3 +78,4 @@ console.log(
 //     gender: "male"
 //   }
 // ]
+
